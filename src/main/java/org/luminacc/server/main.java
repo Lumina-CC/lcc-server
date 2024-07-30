66f3c49d6ab85adf83662fe5573e83a2d1c1a8ad
WindClan <monstertoast2@gmail.com>
2024-07-30 19:39:46 -0400

    first version

---
+++ src/main/java/org/luminacc/server/main.java
@@ -0,0 +1,14 @@
+package org.luminacc.server;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class main implements ModInitializer {
+
+    public static final Logger log = Logger.getLogger("LccServer");
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/luminacc/server/mixins/WirelessModemPeripheralMixin.java
@@ -0,0 +1,53 @@
+package org.luminacc.server.mixins;
+
+import dan200.computercraft.shared.config.Config;
+import dan200.computercraft.shared.peripheral.modem.ModemPeripheral;
+import dan200.computercraft.shared.peripheral.modem.ModemState;
+import dan200.computercraft.shared.peripheral.modem.wireless.WirelessModemPeripheral;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static java.util.Objects.isNull;
+import static org.luminacc.server.main.log;
+
+@Mixin(WirelessModemPeripheral.class)
+public abstract class WirelessModemPeripheralMixin extends ModemPeripheral {
+    protected WirelessModemPeripheralMixin(ModemState state) {
+        super(state);
+    }
+    @Shadow @Final private boolean advanced;
+
+    /**
+     * @author Featherwhisker
+     * @reason Nerfing ender modems
+     */
+    @Overwrite(remap = false)
+    public boolean isInterdimensional() {
+        if (advanced || !getLevel().isThundering()) {
+            return true;
+        }
+        return false;
+    }
+    @Inject(method = "getRange",at=@At("HEAD"),remap = false,cancellable = true)
+    public void getRange(CallbackInfoReturnable<Double> ci) {
+        if (advanced || !isNull(getLevel())) {
+            double returnValue = Config.modemHighAltitudeRange * 2;
+            if (getLevel().isThundering()) {
+                returnValue = Config.modemHighAltitudeRangeDuringStorm * 2;
+            } else if (getLevel().isRaining()) {
+                returnValue = Config.modemHighAltitudeRange ;
+            }
+            //log.info(String.valueOf(returnValue));
+            ci.setReturnValue(returnValue);
+            ci.cancel();
+        } else if (isNull(getLevel())) {
+            ci.setReturnValue((double) 0);
+            ci.cancel();
+        }
+    }
+}
