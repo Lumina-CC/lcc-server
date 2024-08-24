c2973dc36c4a73e72f9bd0d9546df7e17c4a6f88
scmcgowen <scmcgowen@gmail.com>
2024-08-23 21:08:45 -0700

    feat: command blocks/computers can be used by anyone with the minecraft.useopblocks permission

--- src/main/java/org/luminacc/server/main.java
+++ src/main/java/org/luminacc/server/main.java
@@ -2,15 +2,15 @@ package org.luminacc.server;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import dan200.computercraft.shared.computer.blocks.ComputerBlockEntity;
 import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.logging.Logger;
 
 import static net.minecraft.server.command.CommandManager.*;
 public class main implements ModInitializer {
 
-    public static final Logger log = Logger.getLogger("LccServer");
+    public static final Logger log = LoggerFactory.getLogger("lcc-server");
 
     @Override
     public void onInitialize() {
@@ -19,6 +19,5 @@ public class main implements ModInitializer {
                 context.getSource().sendFeedback(() -> Text.literal("Hello, world!"), true);
                 return 1;
             })));
-
     }
 }
---
+++ src/main/java/org/luminacc/server/mixins/CommandBlockPermissionMixin.java
@@ -0,0 +1,29 @@
+package org.luminacc.server.mixins;
+
+import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerAbilities;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class CommandBlockPermissionMixin  extends LivingEntity {
+    private PlayerAbilities abilities;
+    protected CommandBlockPermissionMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+    @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z",at=@At("HEAD"),cancellable = true)
+    public void isCreativeLevelTwoOp(CallbackInfoReturnable<Boolean> cir) {
+        PlayerEntity plr = (PlayerEntity)(Object)this;
+        if (plr.getWorld().isClient) {cir.setReturnValue(true);}
+        else{
+        cir.setReturnValue(plr.getAbilities().creativeMode && (this.getPermissionLevel() >= 2 || Permissions.check(plr,"minecraft.useopblocks")));
+        }
+    }
+
+}
